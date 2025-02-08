71fcdf465e7f2444d8134861d0c9127bc9d16d99
McAlec <hello@mcalec.dev>
2025-02-07 22:54:06 -0500

    Update repo names

--- online/online.ps1
+++ online/online.ps1
@@ -1,9 +1,9 @@
 $tempFolder = "$($env:TEMP)\gdl-pwsh"
 New-Item -Path $tempFolder -ItemType Directory -Force | Out-Null
 New-Item -Path "$($tempFolder)\modules" -ItemType Directory -Force | Out-Null
-Invoke-WebRequest -Uri "https://git.mcalec.dev/gallerydl-pwsh/modules/variables.ps1" -OutFile $tempFolder\modules\variables.ps1 | Out-Null
-Invoke-WebRequest -Uri "https://git.mcalec.dev/gallerydl-pwsh/modules/testforwt.ps1" -OutFile $tempFolder\modules\testforwt.ps1 | Out-Null
-Invoke-WebRequest -Uri "https://git.mcalec.dev/gallerydl-pwsh/run.ps1" -OutFile $tempFolder\run.ps1
+Invoke-WebRequest -Uri "https://git.mcalec.dev/gdl-pwsh/modules/variables.ps1" -OutFile $tempFolder\modules\variables.ps1 | Out-Null
+Invoke-WebRequest -Uri "https://git.mcalec.dev/gdl-pwsh/modules/testforwt.ps1" -OutFile $tempFolder\modules\testforwt.ps1 | Out-Null
+Invoke-WebRequest -Uri "https://git.mcalec.dev/gdl-pwsh/run.ps1" -OutFile $tempFolder\run.ps1
 Import-Module $tempFolder\testforwt.ps1
 Import-Module $tempFolder\variables.ps1
 Import-Module $tempFolder\run.ps1
